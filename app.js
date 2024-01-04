516afab3bb396ccd09b5acb2fcdf7e7d8b6249e3
Gowtham Yarlagadda <gowtham777y@gmail.com>
2024-01-05 03:17:24 +0530

    Add Style to blog create page and retreive data

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
 const port = 3000;
 
 app.get("/", (req, res) =>{
@@ -9,6 +12,11 @@ app.get("/", (req, res) =>{
 
 app.get("/create", (req, res) => {
     res.render("create-blog.ejs");
+})
+
+app.post("/submit", (req,res) => {
+    console.log(req.body["blogTitle"]);
+    res.redirect("/");
 })
 
 app.listen(port, () => {
