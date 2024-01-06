c7b2e72bc26da262d3b12326fa619004ee178bd4
Gowtham Yarlagadda <gowtham777y@gmail.com>
2024-01-07 02:45:06 +0530

    Store blogs, display list in home & a seperate blog page

--- app.js
+++ app.js
@@ -5,20 +5,35 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: false}));
 const port = 3000;
+var blogs = [];
+
 
 app.get("/", (req, res) =>{
-    res.render("landing-page.ejs");
+    res.render("landing-page.ejs", {blogs: blogs});
 })
 
 app.get("/create", (req, res) => {
     res.render("create-blog.ejs");
 })
 
+app.get("/:id", (req,res) => {
+    res.render("blog.ejs",{blogData: blogs[req.params.id]});
+})
+
 app.post("/submit", (req,res) => {
-    console.log(req.body["blogTitle"]);
+    const currentBlog = new blog(req.body["blogAuthor"],req.body["blogTitle"],req.body["blogContent"]);
+    blogs.push(currentBlog);
     res.redirect("/");
 })
 
 app.listen(port, () => {
     console.log(`Server started to listen on port ${port}`);
 })
+
+class blog {
+    constructor (author, title, content){
+        this.author = author;
+        this.title = title;
+        this.content = content;
+    }
+}
